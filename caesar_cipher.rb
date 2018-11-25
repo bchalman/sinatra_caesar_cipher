6493abdd3dc07ace79860bf9083c2dd5d2c700cb
bchalman <blakechalman@gmail.com>
2018-11-25 14:16:10 -0800

    Add caesar_cipher logic

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -8,3 +8,15 @@ get '/' do
   erb :index
 
 end
+
+def caesar_cipher (string, shift)
+  string.split("").collect do |char|
+    num = char.ord
+    if num.between?(64, 91)
+      num = ((num + shift - 65) % 26) + 65
+    elsif num.between?(96, 123)
+      num = ((num + shift - 97) % 26) + 97
+    end
+    char = num.chr
+  end.join
+end
